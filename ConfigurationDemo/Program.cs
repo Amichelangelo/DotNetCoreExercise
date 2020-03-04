e4232d8ad85690b1b305f616044c77060a909300
yin_q <NLyq181130>
2020-03-04 10:28:41 +0800

    添加项目文件。

---
+++ ConfigurationDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConfigurationCommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
