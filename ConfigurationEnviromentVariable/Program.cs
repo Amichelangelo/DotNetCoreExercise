ad74a3fdaeae7d80ea625067cf99d1e5cfebeb72
yin_q <NLyq181130>
2020-03-15 15:37:38 +0800

    配置相关demo

---
+++ ConfigurationEnviromentVariable/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigurationEnviromentVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddEnvironmentVariables("pre_");
+            var root = configurationBuilder.Build();
+            Console.WriteLine($"key1:{root["key1"]}");
+        }
+    }
+}
---
+++ ConfigurationFile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+
+namespace ConfigurationFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json");
+            builder.AddIniFile("appsettings.ini");
+            var root = builder.Build();
+            Console.WriteLine($"kini:{root["keyini"]}");
+            Console.WriteLine($"kjs1:{root["kjs1"]}");
+            IChangeToken changeToken = root.GetReloadToken();
+
+            ChangeToken.OnChange(()=>root.GetReloadToken(),()=>
+            {
+                Console.WriteLine($"restart");
+                Console.WriteLine($"kjs1:{root["kjs1"]}");
+            });
+                
+
+            Console.WriteLine($"start");
+            Console.WriteLine($"kjs1:{root["kjs1"]}");
+
+            Console.ReadKey();
+
+            Console.WriteLine($"restart");
+            Console.WriteLine($"kjs1:{root["kjs1"]}");
+
+        }
+    }
+}
