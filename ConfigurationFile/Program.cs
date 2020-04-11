cbad4d2cfde61cc726e6ed0b587e596a01c5db66
殷琪 <dumuqiaotime@outlook.com>
2020-04-11 18:21:50 +0800

    启动顺序以及Ioc

--- ConfigurationFile/Program.cs
+++ ConfigurationFile/Program.cs
@@ -9,28 +9,26 @@ namespace ConfigurationFile
         static void Main(string[] args)
         {
             var builder = new ConfigurationBuilder();
-            builder.AddJsonFile("appsettings.json");
-            builder.AddIniFile("appsettings.ini");
+            builder.AddJsonFile("appsettings.json", optional:true, reloadOnChange:true);
+            //builder.AddIniFile("appsettings.ini");
             var root = builder.Build();
-            Console.WriteLine($"kini:{root["keyini"]}");
-            Console.WriteLine($"kjs1:{root["kjs1"]}");
+            var config = new Config();
+            root.Bind( config);
+            Console.WriteLine(config.Key6);
             IChangeToken changeToken = root.GetReloadToken();
-
-            ChangeToken.OnChange(()=>root.GetReloadToken(),()=>
-            {
-                Console.WriteLine($"restart");
-                Console.WriteLine($"kjs1:{root["kjs1"]}");
-            });
-                
-
-            Console.WriteLine($"start");
-            Console.WriteLine($"kjs1:{root["kjs1"]}");
-
+            _= ChangeToken.OnChange(() => root.GetReloadToken(),
+                () => { Console.WriteLine($"kjs1:{root["kjs1"]}"); });
+            
             Console.ReadKey();
-
-            Console.WriteLine($"restart");
-            Console.WriteLine($"kjs1:{root["kjs1"]}");
-
+            Console.ReadKey();
         }
+    }
+
+    class Config
+    {
+        public string kjs1 { get; set; }
+
+        public bool Key5 { get; set; }
+        public int Key6 { get; set; }
     }
 }
